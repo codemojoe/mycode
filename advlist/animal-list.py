042595270c4ffde1de36c290cb4dd57b0b3ffc7a
codemojoe <chrisjsavella@gmail.com>
2023-01-10 18:20:22 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main():
+    # create a list called list1
+    listAnimal = ["Bear", "Wolf", "Deer"]
+
+    # display list1
+    print(listAnimal)
+
+    listAnimal.reverse()
+main()
+
