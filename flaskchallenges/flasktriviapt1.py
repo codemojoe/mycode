efc5f84d0fca3884cda74c197e41cde42cbdf816
codemojoe <chrisjsavella@gmail.com>
2023-01-20 22:59:43 +0000

    flask trivia challenge pt 2

--- flaskchallenges/flasktriviapt1.py
+++ flaskchallenges/flasktriviapt1.py
@@ -14,24 +14,31 @@ pic_location= "https://static.alta3.com/courses/api/lec_flaskcontrol_python/dont
 data = requests.get(URL).json()
 results = data["results"][0]
 question = results["question"]
+
+incorrect_answers = []
+for incorrect_answer in results["incorrect_answers"]:
+    incorrect_answers.append(incorrect_answer)
+
 answer = results["correct_answer"]
+
+#username = input("What is your name?\n")
 
 @app.route("/<username>")
 def index(username):
-    return render_template("flasktriviapt1.html", name = username, pic= pic_location, question = question, answer = answer)
+    return render_template("flasktriviapt1.html", name = username, pic= pic_location, question = question, answer1 = incorrect_answers[0], answer2 = incorrect_answers[1], answer3 = incorrect_answers[2],  answer = answer)
 
 @app.route("/login", methods=["POST"])
 def login():
     user_answer = request.form.get("answer")
-    redirect("/outcome/<username>")
-    #if user_answer == answer:
-        #return redirect("/correct")
-    #else:
-        #return redirect("/<username>")
+    #return redirect("/outcome")
+    if user_answer == answer:
+        return redirect("/outcome")
+    else:
+        return redirect("/<username>")
 
-@app.route("/outcome/<username>")
-def correct(username):
-    return render_template("flasktriviaoutcome.html", name = username, pic= pic_location)
+@app.route("/outcome")
+def correct():
+    return render_template("flasktriviaoutcome.html", pic= pic_location)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2224)
