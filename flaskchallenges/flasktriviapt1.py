bfb2f23f62b2b00d11db77e52defba4858a88afd
codemojoe <chrisjsavella@gmail.com>
2023-01-20 21:03:27 +0000

    triviapt2

--- flaskchallenges/flasktriviapt1.py
+++ flaskchallenges/flasktriviapt1.py
@@ -3,26 +3,35 @@
 Simple flask application using redirect()"""
 
 from flask import Flask, render_template, url_for, request, redirect
+import requests
+
+URL = "https://opentdb.com/api.php?amount=1"
 
 app = Flask(__name__)
 
 pic_location= "https://static.alta3.com/courses/api/lec_flaskcontrol_python/dont-panic.png"
 
-@app.route("/")
-def index():
-    return render_template("flasktriviapt1.html", pic= pic_location)
+data = requests.get(URL).json()
+results = data["results"][0]
+question = results["question"]
+answer = results["correct_answer"]
+
+@app.route("/<username>")
+def index(username):
+    return render_template("flasktriviapt1.html", name = username, pic= pic_location, question = question, answer = answer)
 
 @app.route("/login", methods=["POST"])
 def login():
-    answer = request.form.get("answer")
-    if answer == "42":
-        return redirect("/correct")
-    else:
-        return redirect("/")
+    user_answer = request.form.get("answer")
+    redirect("/outcome/<username>")
+    #if user_answer == answer:
+        #return redirect("/correct")
+    #else:
+        #return redirect("/<username>")
 
-@app.route("/correct")
-def correct():
-    return "Correct! The answer was 42."
+@app.route("/outcome/<username>")
+def correct(username):
+    return render_template("flasktriviaoutcome.html", name = username, pic= pic_location)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2224)
