9e17e0b4c6b82f1d375206d872eedfc06c500284
codemojoe <chrisjsavella@gmail.com>
2023-01-20 15:35:57 +0000

    trivia pt 1

--- flaskchallenges/flasktriviapt1.py
+++ flaskchallenges/flasktriviapt1.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python3
-"""DEMO: receiving JSON"""
+#!/usr/bin/python3
+"""Alta3 Research
+Simple flask application using redirect()"""
 
-from flask import Flask
-from flask import redirect
-from flask import request
-from flask import render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-def main():
+pic_location= "https://static.alta3.com/courses/api/lec_flaskcontrol_python/dont-panic.png"
 
-    @app.route("/")
-    def trivia_form():
-        return render_template("flasktriviapt1.html", name = username)
+@app.route("/")
+def index():
+    return render_template("flasktriviapt1.html", pic= pic_location)
 
+@app.route("/login", methods=["POST"])
+def login():
+    answer = request.form.get("answer")
+    if answer == "42":
+        return redirect("/correct")
+    else:
+        return redirect("/")
 
-
+@app.route("/correct")
+def correct():
+    return "Correct! The answer was 42."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2224)
-    main()
